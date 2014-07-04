5ad2b82945dde11a0b46c3b81e9105056eff6b4a
mriehl <max@riehl.io>
2014-07-04 14:13:10 +0200

    relaxing the rpm dependency
    The repoclient now works without `rpm` bindings, but it will crash while
    performing the query command if the bindings are not present.

--- src/main/python/yum_repo_client/rpmsearch.py
+++ src/main/python/yum_repo_client/rpmsearch.py
@@ -1,5 +1,4 @@
 from yum_repo_client.rpmfile import RpmFile
-import rpm
 import re
 
 class RpmSearch():
@@ -11,27 +10,28 @@ class RpmSearch():
 
     def _sort_rpms(self, rpm_list, sort_desc):
         grouped_rpms = {}
-        
+
         for rpm_file in rpm_list:
             if rpm_file.name in grouped_rpms:
                 grouped_rpms.get(rpm_file.name).append(rpm_file)
             else:
                 grouped_rpms[rpm_file.name] = [rpm_file]
-        
+
         keys = grouped_rpms.keys()
         for key in keys:
             grouped_rpms.get(key).sort(cmp=self._compare_rpms_by_version, reverse=sort_desc)
-          
+
         keys.sort(reverse=sort_desc)
-        
+
         sorted_list = []
         for key in keys:
             sorted_list.extend(grouped_rpms.get(key))
-        
+
         return sorted_list
-    
+
 
     def _compare_rpms_by_version(self, rpm_file1, rpm_file2):
+        import rpm
         if rpm_file1.name != rpm_file2.name:
             return rpm_file1.name < rpm_file2.name
         return rpm.labelCompare(('1', rpm_file1.version, rpm_file1.release), ('1', rpm_file2.version, rpm_file2.release))
